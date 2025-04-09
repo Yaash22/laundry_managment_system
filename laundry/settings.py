44ce4764c05ed746c4ba899b901ed4bc6ebc63cb
Yash A U <140700061+Yaash22@users.noreply.github.com>
2025-04-09 23:52:36 +0530

    Update settings.py

--- laundry/settings.py
+++ laundry/settings.py
@@ -146,10 +146,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'yashallampalli@gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'xyz@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = 'hznw oxlv jvtn smqi'
 
 
